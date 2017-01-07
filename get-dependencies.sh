524e91a179c5f1aed75653cbb52050c691604a03
juju4 <juju4@users.noreply.github.com>
2017-01-07 04:37:55 -0500

    sync repositories

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,4 +6,10 @@ umask 022
 rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.jenkinsslave ] && ln -s ansible-jenkinsslave $rolesdir/juju4.jenkinsslave
+[ ! -e $rolesdir/juju4.jenkinsslave ] && cp -R $rolesdir/ansible-jenkinsslave $rolesdir/juju4.jenkinsslave
+
+## don't stop build on this script return code
+true
 
